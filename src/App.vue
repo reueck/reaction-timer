549142e99b27f0c26f4001d89734cb7a2470f73e
reueck <65670634+reueck@users.noreply.github.com>
2023-02-01 22:22:41 +0100

    Created blob and added hooks to show the blob when the user clicks on the start button

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,84 @@
-
-
 <template>
-        
-
-           
-  
-  <header>
-    <h1>Reakcją tajmer</h1>
-    <div>
-      <button @click="start">Nowa gra</button>
-
-    </div>
-  </header>
-
+	<header>
+		<h1>Reakcją tajmer</h1>
+		<div>
+      	<Blob v-if="isPlaying" :delay="delay" />
+			<button @click="start" :disabled="isPlaying">Nowa gra</button>
+			<!-- dopóki gra jest grana to przycisk jest dezaktywowany a powinien być niewidzoczny -->
+		</div>
+	</header>
 
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    components: {
-      
-    },
-    data(){
-      return{
-        isPlaying: false,
-        delay: null
-      }
-    },
-    methods: {
-      start() {
-        this.isPlaying = true;
-        this.delay = 2000 + Math.random() * 5000;
-        // to da nam randomową liczbę między 2 a 5 sekund
-        console.log(this.delay);
-      }
-    },
-  }
+import Blob from './components/Blob.vue'
+export default {
+	name: 'App',
+	components: {
+		Blob,
+	},
+	data() {
+		return {
+			isPlaying: false,
+			delay: null,
+		}
+	},
+	methods: {
+		start() {
+			this.isPlaying = true
+			this.delay = 2000 + Math.random() * 5000
+		},
+	},
+}
 </script>
 
-
 <style lang="scss" scoped>
-
-
 header {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: var(--section-gap);
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: var(--section-gap);
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-  }
+	header {
+		display: flex;
+		place-items: center;
+	}
 }
 
-h1, button {
-  font-size: 6rem;
-  font-weight: bold;
-  letter-spacing: 0.6rem;
-  background: linear-gradient(90deg, #FC466B 0%, #3F5EFB 100%);
-  background-clip: text;
-  --webkit-text-fill-color: transparent;
-  letter-spacing: -0.04em;
-  user-select: none;
-  text-shadow: -15px 5px 20px rgba(61, 0, 77, 0.38);
-  margin: 0;
-  text-align: center;
-  font-family: Bangers, cursive;
+h1,
+button {
+	font-size: 6rem;
+	font-weight: bold;
+	letter-spacing: 0.6rem;
+	background: linear-gradient(90deg, #fc466b 0%, #3f5efb 100%);
+	background-clip: text;
+	--webkit-text-fill-color: transparent;
+	letter-spacing: -0.04em;
+	user-select: none;
+	text-shadow: -15px 5px 20px rgba(61, 0, 77, 0.38);
+	margin: 0;
+	text-align: center;
+	font-family: Bangers, cursive;
 }
 
 button {
-  font-size: 4rem;
-  color: white;
-  background: linear-gradient(90deg, #FC466B 0%, #3F5EFB 100%);
-  background-clip: text;
-  --webkit-text-fill-color: transparent;
-  letter-spacing: -0.04em;
-  border: none;
-  padding: 1rem 2rem;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  &:hover {
-    transform: scale(1.1);
-  }
-  &:active {
-    transform: scale(0.9);
-  }
+	font-size: 4rem;
+	color: white;
+	background: linear-gradient(90deg, #fc466b 0%, #3f5efb 100%);
+	background-clip: text;
+	--webkit-text-fill-color: transparent;
+	letter-spacing: -0.04em;
+	border: none;
+	padding: 1rem 2rem;
+	cursor: pointer;
+	transition: all 0.2s ease-in-out;
+	&:hover {
+		transform: scale(1.1);
+	}
+	&:active {
+		transform: scale(0.9);
+	}
 }
 </style>
---
+++ src/components/Blob.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="blob" v-if="showBlob">click me xd</div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+data() {
+    return {
+        showBlob: false
+    }
+},
+mounted(){
+    setTimeout(() => {
+        this.showBlob = true
+        console.log(this.delay);
+    }, this.delay)},
+updated() {
+    console.log('updated');
+},
+unmounted() {
+    this.showBlob = false
+},
+// to się odpali dopiero jak to będzie zmountowane do dom
+}
+</script>
+
+<style lang="scss">
+.blob {
+	width: 200px;
+	height: 50px;
+	background-color: red;
+	border-radius: 50%;
+	text-align: center;
+	padding: 5px 0;
+	margin: 40px auto;
+}
+</style>
